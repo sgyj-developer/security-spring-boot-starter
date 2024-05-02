74ae27cec39046fdd4ec19fdc6af67636eba85a4
이승구 <leesg107@naver.com>
2024-05-02 15:23:13 +0900

    feat: first commit

---
+++ src/main/java/com/yeseung/sgyjsecurityspringbootstarter/SgyjSecuritySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.yeseung.sgyjsecurityspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgyjSecuritySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgyjSecuritySpringBootStarterApplication.class, args);
+    }
+
+}
