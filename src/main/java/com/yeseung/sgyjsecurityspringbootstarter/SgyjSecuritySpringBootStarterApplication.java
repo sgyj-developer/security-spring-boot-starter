fd90608da49ae2b1625ef70eab669088e9b2fa76
이승구 <leesg107@naver.com>
2024-06-01 10:32:15 +0900

    feat:ConfigurationPropertiesScan 옵션 설정

--- src/main/java/com/yeseung/sgyjsecurityspringbootstarter/SgyjSecuritySpringBootStarterApplication.java
+++ src/main/java/com/yeseung/sgyjsecurityspringbootstarter/SgyjSecuritySpringBootStarterApplication.java
@@ -2,8 +2,10 @@ package com.yeseung.sgyjsecurityspringbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SgyjSecuritySpringBootStarterApplication {
 
     public static void main(String[] args) {
